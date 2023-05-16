a13735638c68fa640f54025fcf296b1659f6ac55
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-16 11:39:15 -0300

    updated tailwind config, add colors in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
+      colors: {
+         trade_tech: {
+            blue_ncs: '#008BC6',
+            bright_gray: '#E6F4F1',
+            cool_black: '#002A5A',
+            marigold: '#E79C25',
+         },
+      },
       extend: {},
    },
    plugins: [],
