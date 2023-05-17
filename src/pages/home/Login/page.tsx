ef6fafe89c187bf6c79007f6750cb2f8da5552b2
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-17 09:57:33 -0300

    created a login section

---
+++ src/pages/home/Login/page.tsx
@@ -0,0 +1,27 @@
+const Login = () => {
+   return (
+      <section>
+         <form>
+            <div>
+               <label>
+                  <legend>Login</legend>
+                  <input
+                     type="text"
+                     name=""
+                     id=""
+                     placeholder="Entre com a key"
+                  />
+               </label>
+               <select name="" id="">
+                  <option value="">Selecione o país</option>
+                  <option value="brasil">Brasil</option>
+                  <option value="espanha">Espanha</option>
+                  <option value="alemanha">Alemanha</option>
+               </select>
+            </div>
+         </form>
+      </section>
+   );
+};
+
+export default Login;
