23535f1fb45bcb4865cd90b38bff8d7ab25eb638
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-17 09:49:50 -0300

    updated intro to home

--- src/pages/home/intro_home/intro.tsx
+++ src/pages/home/intro_home/intro.tsx
@@ -1,8 +1,12 @@
 const Intro_home = () => {
    return (
-      <div>
-         <h1>Meu time</h1>
-         <p>Seja bem vindo ao seu portal do futebol</p>
+      <div className="text-center w-full">
+         <h1 className="text-9xl font-sans text-trade_tech-cool_black">
+            Meu time
+         </h1>
+         <p className="text-2xl text-trade_tech-blue_ncs">
+            Seja bem vindo ao seu portal do futebol
+         </p>
       </div>
    );
 };
