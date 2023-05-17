6bf9c9b85b6c1e743726e1c96670e9c9560986b9
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-17 09:34:43 -0300

    updated header

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -1,13 +1,7 @@
 const Header = () => {
    return (
-      <header className="flex h-20 border-b border-b-trade_tech-bright_gray justify-between items-center px-10">
+      <header className="flex h-20 border-b border-b-trade_tech-bright_gray justify-center items-center">
          <h1 className="text-6xl font-sans">Meu Time</h1>
-
-         <div>
-            <button className="border border-trade_tech-cool_black py-1 px-2 rounded font-sans text-xl text-trade_tech-cool_black">
-               Entrar / Cadastrar
-            </button>
-         </div>
       </header>
    );
 };
