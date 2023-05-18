d64b274316e8cf3a4e89d9595c03ea3572c03a8d
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-18 11:28:20 -0300

    updated home page

--- src/components/global/Footer/index.tsx
+++ src/components/global/Footer/index.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-   return <div>index</div>;
+   return (
+      <footer className="border-t border-t-trade_tech-blue_ncs/5 py-[3.6vh] text-center">
+         <span className="text-trade_tech-blue_ncs/60">
+            Todos os direitos reservados
+         </span>
+      </footer>
+   );
 };
 
 export default Footer;
--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
    return (
       <header className="flex h-20 border-b border-b-trade_tech-bright_gray justify-center items-center">
-         <h1 className="text-6xl font-sans">Meu Time</h1>
+         <h1 className="text-6xl font-sans text-trade_tech-cool_black">
+            Meu Time
+         </h1>
       </header>
    );
 };
